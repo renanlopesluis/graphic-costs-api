735ef823247d75c63a9a9d9abca71ce9c563ddb2
renanlopesluis <renanlopesluis@gmail.com>
2021-12-07 11:47:11 -0300

    Adjusts env

--- src/config/bd.js
+++ src/config/bd.js
@@ -1,5 +1,7 @@
-const user = 'renanl';
-const password = encodeURIComponent('Bl4ckH0l3Sun');
+require('dotenv').config();
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+
 const connectionUrl = `mongodb+srv://${user}:${password}@cluster0.gadqx.mongodb.net/apiDataBase?retryWrites=true&w=majority`;
 const mongoose = require('mongoose');
 module.exports = {
